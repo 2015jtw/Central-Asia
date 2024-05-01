9bd55fe58866bf47d245468578c7a05eda82b4f8
Josh Worden <josh.worden@arcticleaf.io>
2024-04-30 23:05:36 -0400

    adding shadcn components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
+//import components
 import Header from "@/components/Header";
 import HomepageCarousel from "@/components/homepage-carousel";
+import {
+	Carousel,
+	CarouselContent,
+	CarouselItem,
+	CarouselNext,
+	CarouselPrevious,
+} from "@/components/ui/carousel";
+import { Card, CardContent } from "@/components/ui/card";
 import TourCard from "@/components/tourCard";
 import Wrapper from "@/components/Wrapper";
+
+//import other dependencies
+import Image from "next/image";
 
 export default function Home() {
 	// TODO: refactor this in a data file and just map over it
@@ -49,7 +61,7 @@ export default function Home() {
 	];
 
 	return (
-		<main className="bg-white flex min-h-screen flex-col">
+		<main className="bg-white flex h-[500px] flex-col">
 			<Header />
 
 			<HomepageCarousel
@@ -58,6 +70,35 @@ export default function Home() {
 						irure qui lorem cupidatat commodo. Elit sunt amet fugiat
 						veniam occaecat fugiat aliqua."
 			/>
+
+			<div className="flex items-center justify-center h-screen">
+				<Carousel className="w-full max-w-[800px] mx-0">
+					<CarouselContent>
+						{Array.from({ length: 5 }).map((_, index) => (
+							<CarouselItem key={index}>
+								<div className="p-1">
+									<Card>
+										<CardContent className="flex flex-col aspect-square items-center justify-center p-6">
+											<h1>test</h1>
+											<Image
+												src="/Uzbekistan-at-sunset.jpg"
+												className="h-[50%]"
+												width={400}
+												height={400}
+												objectFit="cover"
+												objectPosition="center"
+												alt="Uzbekistan at sunset"
+											/>
+										</CardContent>
+									</Card>
+								</div>
+							</CarouselItem>
+						))}
+					</CarouselContent>
+					<CarouselPrevious />
+					<CarouselNext />
+				</Carousel>
+			</div>
 
 			<Wrapper>
 				<div
