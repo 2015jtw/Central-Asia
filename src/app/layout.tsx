4ba6b01dd41880dc8221cb5d630e9763cba9e8f7
Josh Worden <josh.worden@arcticleaf.io>
2024-05-09 22:22:12 -0400

    creating contact form

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,36 @@
-import type { Metadata } from "next";
-import { Inter, Fira_Sans } from "next/font/google";
-import Head from "next/head";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Inter, Fira_Sans } from 'next/font/google';
+import Head from 'next/head';
+import './globals.css';
+import Header from '@/components/Header';
 
 // const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-	title: "Central Asia Tours",
-	description: "Travel Agency based in Central Asia",
+    title: 'Central Asia Tours',
+    description: 'Travel Agency based in Central Asia',
 };
 
 const firaSans = Fira_Sans({
-	subsets: ["latin"],
-	display: "swap",
-	weight: ["300", "400"],
+    subsets: ['latin'],
+    display: 'swap',
+    weight: ['300', '400'],
 });
 
 export default function RootLayout({
-	children,
+    children,
 }: Readonly<{
-	children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-	return (
-		<html lang="en">
-			<body className={firaSans.className}>{children}</body>
-		</html>
-	);
+    return (
+        <html lang="en">
+            <body
+                className={firaSans.className}
+                suppressHydrationWarning={true}
+            >
+                <Header />
+                {children}
+            </body>
+        </html>
+    );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,93 +1,206 @@
-//import components
-import Header from "@/components/Header";
-import HomepageCarousel from "@/components/homepage-carousel";
-import TourCard from "@/components/tourCard";
-import Wrapper from "@/components/Wrapper";
+'use client';
 
-//import other dependencies
-import Image from "next/image";
+//import components
+import Header from '@/components/Header';
+import HomepageCarousel from '@/components/homepage-carousel';
+import TourCard from '@/components/tourCard';
+import Wrapper from '@/components/Wrapper';
+
+// form/shadcn dependencies
+import {
+    Form,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+    FormControl,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { Button } from '@/components/ui/button';
+import { Textarea } from '@/components/ui/textarea';
+
+// nextjs dependencies
+import Image from 'next/image';
+
+const formSchema = z.object({
+    firstName: z.string().min(2).max(50),
+    lastName: z.string().min(2).max(50),
+    email: z.string().email(),
+    message: z.string().min(10).max(500),
+});
 
 export default function Home() {
-	// TODO: refactor this in a data file and just map over it
-	const tourData = [
-		{
-			title: "Khiva",
-			image: "/jonathan-greenaway-HDK1NstWkcI-unsplash.jpg",
-			description:
-				"Uzbekistan remains the most-visited country in Central Asia for one key reason.",
-			time: "September 1st - 7th",
-			price: 100,
-		},
-		{
-			title: "BuKhara",
-			image: "/jonathan-greenaway-HDK1NstWkcI-unsplash.jpg",
-			description:
-				"Uzbekistan remains the most-visited country in Central Asia for one key reason.",
-			time: "September 1st - 7th",
-			price: 100,
-		},
-		{
-			title: "Samarkand",
-			image: "/jonathan-greenaway-HDK1NstWkcI-unsplash.jpg",
-			description:
-				"Uzbekistan remains the most-visited country in Central Asia for one key reason.",
-			time: "September 1st - 7th",
-			price: 100,
-		},
-		// {
-		// 	title: "Samarkand",
-		// 	image: "/jonathan-greenaway-HDK1NstWkcI-unsplash.jpg",
-		// 	description:
-		// 		"Uzbekistan remains the most-visited country in Central Asia for one key reason",
-		// 	time: "September 1st - 7th",
-		// 	price: 100,
-		// },
-		// {
-		// 	title: "Samarkand",
-		// 	image: "/jonathan-greenaway-HDK1NstWkcI-unsplash.jpg",
-		// 	description:
-		// 		"Uzbekistan remains the most-visited country in Central Asia for one key reason",
-		// 	time: "September 1st - 7th",
-		// 	price: 100,
-		// },
-	];
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            firstName: '',
+            lastName: '',
+            email: '',
+            message: '',
+        },
+    });
 
-	return (
-		<main className="bg-white flex h-[500px] flex-col">
-			<Header />
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        console.log(values);
+    }
 
-			<HomepageCarousel
-				heading="Welcome to Central Asia Tours"
-				description="Anim aute id magna aliqua ad ad non deserunt sunt. Qui
+    // TODO: refactor this in a data file and just map over it
+    const tourData = [
+        {
+            title: 'Khiva',
+            image: '/jonathan-greenaway-HDK1NstWkcI-unsplash.jpg',
+            description:
+                'Uzbekistan remains the most-visited country in Central Asia for one key reason.',
+            time: 'September 1st - 7th',
+            price: 100,
+        },
+        {
+            title: 'BuKhara',
+            image: '/jonathan-greenaway-HDK1NstWkcI-unsplash.jpg',
+            description:
+                'Uzbekistan remains the most-visited country in Central Asia for one key reason.',
+            time: 'September 1st - 7th',
+            price: 100,
+        },
+        {
+            title: 'Samarkand',
+            image: '/jonathan-greenaway-HDK1NstWkcI-unsplash.jpg',
+            description:
+                'Uzbekistan remains the most-visited country in Central Asia for one key reason.',
+            time: 'September 1st - 7th',
+            price: 100,
+        },
+        // {
+        // 	title: "Samarkand",
+        // 	image: "/jonathan-greenaway-HDK1NstWkcI-unsplash.jpg",
+        // 	description:
+        // 		"Uzbekistan remains the most-visited country in Central Asia for one key reason",
+        // 	time: "September 1st - 7th",
+        // 	price: 100,
+        // }
+    ];
+
+    return (
+        <main className="bg-white flex h-[500px] flex-col">
+            <HomepageCarousel
+                heading="Welcome to Central Asia Tours"
+                description="Anim aute id magna aliqua ad ad non deserunt sunt. Qui
 						irure qui lorem cupidatat commodo. Elit sunt amet fugiat
 						veniam occaecat fugiat aliqua."
-			/>
+            />
 
-			<Wrapper>
-				<div
-					id="tours"
-					className="text-black flex flex-col items-center p-8"
-				>
-					<h1 className=" text-4xl font-semibold pb-4 ">
-						Central Asia Tours
-					</h1>
-					<div className="flex flex-col overflow-hidden lg:flex-row justify-center">
-						{tourData.map((tour, index) => {
-							return (
-								<TourCard
-									key={index}
-									title={tour.title}
-									image={tour.image}
-									description={tour.description}
-									time={tour.time}
-									price={tour.price}
-								/>
-							);
-						})}
-					</div>
-				</div>
-				<div className=" h-[800px] bg-red-500"></div>
-			</Wrapper>
-		</main>
-	);
+            <Wrapper>
+                <div
+                    id="tours"
+                    className="text-black flex flex-col items-center p-8"
+                >
+                    <h1 className=" text-4xl font-semibold pb-4 ">
+                        Central Asia Tours
+                    </h1>
+                    <div className="flex flex-col overflow-hidden lg:flex-row justify-center">
+                        {tourData.map((tour, index) => {
+                            return (
+                                <TourCard
+                                    key={index}
+                                    title={tour.title}
+                                    image={tour.image}
+                                    description={tour.description}
+                                    time={tour.time}
+                                    price={tour.price}
+                                />
+                            );
+                        })}
+                    </div>
+                </div>
+
+                <Form {...form}>
+                    <form
+                        onSubmit={form.handleSubmit(onSubmit)}
+                        className=" max-w-md mx-auto my-4 w-full flex flex-col gap-4"
+                    >
+                        <FormField
+                            name="firstName"
+                            control={form.control}
+                            render={({ field }) => {
+                                return (
+                                    <FormItem>
+                                        <FormLabel>First Name</FormLabel>
+                                        <FormControl>
+                                            <Input
+                                                {...field}
+                                                placeholder="First Name"
+                                                type="text"
+                                            />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                );
+                            }}
+                        />
+
+                        <FormField
+                            name="lastName"
+                            control={form.control}
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Last Name</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            {...field}
+                                            placeholder="Last Name"
+                                            type="text"
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+
+                        <FormField
+                            name="email"
+                            control={form.control}
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Email Address</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            {...field}
+                                            placeholder="Email Address"
+                                            type="email"
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+
+                        <FormField
+                            name="message"
+                            control={form.control}
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Message</FormLabel>
+                                    <FormControl>
+                                        <Textarea
+                                            {...field}
+                                            placeholder="Message"
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <Button type="submit" className="w-full">
+                            Submit
+                        </Button>
+                    </form>
+                </Form>
+
+                <div className=" h-[800px] bg-red-500"></div>
+            </Wrapper>
+        </main>
+    );
 }
